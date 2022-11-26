9a1bb2d984f9041d41c71c51c6c6d1b4540ff0a9
Truthliarr <103116722+Truthliarr@users.noreply.github.com>
2022-11-26 11:54:30 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,15 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
+global.Owner = ['96170705350']
+global.OwnerNumber = ['96170705350']
+global.ownertag = ['96170705350']
+global.BotName = "Maybos"
 global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.author = "By: Maybos md"
+global.OwnerName = "Hassan HY"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
-global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
+global.SupportGroupLink = "https://chat.whatsapp.com/Lw4Ue7PQkyGAImWIuzU9ds"
 global.sessionName = "session"
 
 
