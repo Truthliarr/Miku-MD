cfbaec02746127415e55df18a2cb94c0895d8190
Truthliarr <103116722+Truthliarr@users.noreply.github.com>
2022-11-27 15:03:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -58,11 +58,11 @@ global.ntilink = []
 
 
 global.mess = {
-    jobdone: 'Job done...',
-    useradmin: 'Sorry, only *Group Admins* can use this command *Baka*!',
-    botadmin: 'Sorry, i cant execute this command without being an *Admin* of this group.',
-    botowner: 'Only my *Owner* can use this command, Baka!',
-    grouponly: 'This command is only made for *Groups*, Baka!',
+    jobdone: 'Mission accomplished',
+    useradmin: 'Sorry only admins can use rhis command *mesh sorry*',
+    botadmin: 'Lezem koon admin',
+    botowner: 'Bas l owner can use rhe command',
+    grouponly: 'This command is only made for groups Baka!',
     privateonly: 'This command is only made for *Private Chat*, Baka!',
     botonly: 'Only the *Bot itself* can use this command!',
     waiting: 'Waiting...',
